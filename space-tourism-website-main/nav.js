430f327ca0f3483f9f7f734a00e85d763db75074
dubeyanant <anantkumar312@gmail.com>
2023-07-06 16:52:27 +0530

    fix: delete space tourism challenge folder

--- space-tourism-website-main/nav.js
+++
@@ -1,7 +0,0 @@
-const primaryNav = document.querySelector(".header__nav");
-const navToggle = document.querySelector(".header__menu_toggle");
-
-navToggle.addEventListener("click", () => {
-  primaryNav.classList.toggle("translate");
-  navToggle.classList.toggle("change");
-});
